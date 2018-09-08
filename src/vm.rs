5cd6f822cd759fa0e6a02dfdbda3a5d808044c7d
Russell Blickhan <rwblickhan@gmail.com>
2018-09-08 13:55:23 -0700

    Begin implementing VM, in particular adding, subtracting, and integer constants

--- src/vm.rs
+++ src/vm.rs
@@ -1,17 +1,24 @@
 extern crate byteorder;
 
 use self::byteorder::{BigEndian, ReadBytesExt};
-use bytecode::ConstantPoolIdx;
+use bytecode::{Bytecode, ConstantPoolIdx, Opcode, Tag};
 use std::collections::VecDeque;
 use std::fs;
 use std::io::{Cursor, Error};
+
+#[derive(Clone, PartialEq)]
+enum RuntimeObject {
+    Integer(i32),
+    Float(f64),
+    Bool(bool),
+    Nil,
+}
 
 #[derive(Debug)]
 pub enum StartupError {
     MissingMagicNumber,
     InvalidMagicNumber(u8),
     InvalidConstantPoolSize,
-    InvalidConstant(u8),
     FileError(Error),
 }
 
@@ -21,12 +28,27 @@ impl From<Error> for StartupError {
     }
 }
 
+pub enum RuntimeError {
+    InvalidConstant(u8),
+    StackError,
+    FileError(Error),
+    InvalidOperand(String),
+}
+
+impl From<Error> for RuntimeError {
+    fn from(error: Error) -> Self {
+        RuntimeError::FileError(error)
+    }
+}
+
 #[derive(Default)]
 pub struct VM {
     had_exec_error: bool,
     constant_pool_size: ConstantPoolIdx,
-    constant_pool: Vec<u8>,
-    instructions: Vec<u8>,
+    constant_pool: Vec<Bytecode>,
+    instructions: Vec<Bytecode>,
+    pc: usize,
+    stack: Vec<RuntimeObject>,
 }
 
 impl VM {
@@ -53,11 +75,124 @@ impl VM {
         for _i in 0..constant_pool_size {
             constant_pool.push(input_queue.pop_front().unwrap());
         }
-        let mut instructions = Vec::from(input_queue);
-        Ok(VM { had_exec_error: false, constant_pool_size, constant_pool, instructions })
+        let instructions = Vec::from(input_queue);
+        Ok(VM {
+            had_exec_error: false,
+            constant_pool_size,
+            constant_pool,
+            instructions,
+            pc: 0,
+            stack: Vec::with_capacity(2 ^ 16),
+        })
     }
 
-    pub fn run(&mut self) {}
+    pub fn run(&mut self) {
+        loop {
+            match Opcode::from(self.instructions[self.pc]) {
+                Opcode::Nop => self.nop(),
+                Opcode::Halt => return,
+                Opcode::Pop => self.pop(),
+                Opcode::Constant => match self.constant() {
+                    _ => () // TODO
+                },
+                Opcode::Add => {} // TODO
+                Opcode::Sub => {} // TODO
+            };
+        }
+    }
+
+    fn nop(&mut self) {
+        self.pc = self.pc + 1;
+    }
+
+    fn pop(&mut self) {
+        self.pc = self.pc + 1;
+        self.stack.pop();
+    }
+
+    fn constant(&mut self) -> Result<(), RuntimeError> {
+        self.pc = self.pc + 1;
+        let idx = Cursor::new({
+            let mut tmp = Vec::new();
+            for _i in 0..2 {
+                tmp.push(self.instructions[self.pc]);
+                self.pc = self.pc + 1;
+            }
+            tmp
+        }).read_u16::<BigEndian>()?;
+        let tag = Tag::from(self.constant_pool[usize::from(idx)]);
+        match tag {
+            Tag::Nil => Ok(self.stack.push(RuntimeObject::Nil)),
+            Tag::Integer => Ok(self.stack.push(RuntimeObject::Integer(
+                Cursor::new({
+                    let mut tmp = Vec::with_capacity(4);
+                    for i in 1..5 {
+                        tmp.push(self.constant_pool[usize::from(idx + i)]);
+                    }
+                    tmp
+                }).read_i32::<BigEndian>()?
+            ))),
+            _ => { Ok(()) } // TODO
+        }
+    }
+
+    fn add(&mut self) -> Result<(), RuntimeError> {
+        self.pc = self.pc + 1;
+        let right = match self.stack.pop() {
+            Some(val) => val,
+            None => return Err(RuntimeError::StackError),
+        };
+        let left = match self.stack.pop() {
+            Some(val) => val,
+            None => return Err(RuntimeError::StackError),
+        };
+        match left {
+            RuntimeObject::Integer(left_int) => {
+                match right {
+                    RuntimeObject::Integer(right_int) => Ok(self.stack.push(RuntimeObject::Integer(left_int + right_int))),
+                    RuntimeObject::Float(right_float) => Ok(self.stack.push(RuntimeObject::Float(f64::from(left_int) + right_float))),
+                    _ => Err(RuntimeError::InvalidOperand("Right operand to addition not numeric type".to_string()))
+                }
+            }
+            RuntimeObject::Float(left_float) => {
+                match right {
+                    RuntimeObject::Integer(right_int) => Ok(self.stack.push(RuntimeObject::Float(left_float + f64::from(right_int)))),
+                    RuntimeObject::Float(right_float) => Ok(self.stack.push(RuntimeObject::Float(left_float + right_float))),
+                    _ => Err(RuntimeError::InvalidOperand("Right operand to addition not numeric type".to_string()))
+                }
+            }
+            _ => Err(RuntimeError::InvalidOperand("Left operand to addition not numeric type".to_string())),
+        }
+    }
+
+    fn sub(&mut self) -> Result<(), RuntimeError> {
+        self.pc = self.pc + 1;
+        let right = match self.stack.pop() {
+            Some(val) => val,
+            None => return Err(RuntimeError::StackError),
+        };
+        let left = match self.stack.pop() {
+            Some(val) => val,
+            None => return Err(RuntimeError::StackError),
+        };
+        match left {
+            RuntimeObject::Integer(left_int) => {
+                match right {
+                    RuntimeObject::Integer(right_int) => Ok(self.stack.push(RuntimeObject::Integer(left_int - right_int))),
+                    RuntimeObject::Float(right_float) => Ok(self.stack.push(RuntimeObject::Float(f64::from(left_int) - right_float))),
+                    _ => Err(RuntimeError::InvalidOperand("Right operand to addition not numeric type".to_string()))
+                }
+            }
+            RuntimeObject::Float(left_float) => {
+                match right {
+                    RuntimeObject::Integer(right_int) => Ok(self.stack.push(RuntimeObject::Float(left_float - f64::from(right_int)))),
+                    RuntimeObject::Float(right_float) => Ok(self.stack.push(RuntimeObject::Float(left_float - right_float))),
+                    _ => Err(RuntimeError::InvalidOperand("Right operand to addition not numeric type".to_string()))
+                }
+            }
+            _ => Err(RuntimeError::InvalidOperand("Left operand to addition not numeric type".to_string())),
+        }
+    }
 
     pub fn had_error(&self) -> bool {
         self.had_exec_error
